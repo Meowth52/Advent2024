94f766562be55cf6602e48c56462d07abb76d70d
Meowth <NoThisIsAPublicRepo@No.No>
2024-11-22 14:13:51 +0100

    Add project files.

---
+++ Day06.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Advent2024
+{
+    public class Day06 : Day
+    {
+        List<int> Instructions;
+        public Day06(string _input) : base(_input)
+        {
+            string Input = this.CheckFile(_input);
+            Instructions = this.ParseListOfInteger(Input);
+        }
+        public override Tuple<string, string> GetResult()
+        {
+            return Tuple.Create(GetPartOne(), GetPartTwo());
+        }
+        public string GetPartOne()
+        {
+            int ReturnValue = 0;
+
+            return ReturnValue.ToString();
+        }
+        public string GetPartTwo()
+        {
+            int ReturnValue = 0;
+
+            return ReturnValue.ToString();
+        }
+    }
+}
