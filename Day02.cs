79eb72315b32672e947e5294fd1904a1fafd8795
Meowth <NoThisIsAPublicRepo@No.No>
2024-12-02 12:42:30 +0100

    woop

--- Day02.cs
+++ Day02.cs
@@ -9,11 +9,11 @@ namespace Advent2024
 {
     public class Day02 : Day
     {
-        List<int> Instructions;
+        List<List<int>> Instructions;
         public Day02(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfIntegerLists(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,14 +22,51 @@ namespace Advent2024
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            foreach(List<int> report in Instructions)
+            {
+                if (SafetyCheck(report))
+                {
+                    ReturnValue++;
+                }
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            foreach (List<int> report in Instructions)
+            {
+                for(int i = 0; i < report.Count; i++)
+                {
+                    List<int> reprt = new List<int>(report);
+                    reprt.RemoveAt(i);
+                    if (SafetyCheck(reprt))
+                    {
+                        ReturnValue++;
+                        break;
+                    }
+                }
+            }
             return ReturnValue.ToString();
+        }
+        bool SafetyCheck(List<int> report)
+        {
+            int previos = report[0];
+            bool safe = true;
+            int theOne = 1;
+            if (report[1] < previos)
+                theOne = -1;
+            for (int i = 1; i < report.Count; i++)
+            {
+                int diff = (report[i] - previos) * theOne;
+                if (diff != 1 && diff != 2 && diff != 3)
+                {
+                    safe = false;
+                    break;
+                }
+                previos = report[i];
+            }
+            return safe;
         }
     }
 }
