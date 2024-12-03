7f45b32246abaf38282fdbbe2aa12836458d212a
Meowth <NoThisIsAPublicRepo@No.No>
2024-12-03 14:01:29 +0100

    bedo

--- Day03.cs
+++ Day03.cs
@@ -9,11 +9,10 @@ namespace Advent2024
 {
     public class Day03 : Day
     {
-        List<int> Instructions;
+        string Input;
         public Day03(string _input) : base(_input)
         {
-            string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Input = this.CheckFile(_input);//.TrimEnd('\r', '\n');
         }
         public override Tuple<string, string> GetResult()
         {
@@ -21,15 +20,38 @@ namespace Advent2024
         }
         public string GetPartOne()
         {
+            List<(int, int)> Instructions = ParseMul(Input);
             int ReturnValue = 0;
-
+            foreach((int, int) instruction in Instructions)
+            {
+                ReturnValue += instruction.Item1 * instruction.Item2; 
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            string Dodont = "do()" + Input+ "don't()";
+            MatchCollection Matches = Regex.Matches(Dodont, @"(?<=do\(\)).*?(?=don't\(\))",RegexOptions.Singleline);
+            foreach (Match m in Matches)
+            {
+                List<(int, int)> subSet = ParseMul(m.Value);
+                foreach ((int, int) instruction in subSet)
+                {
+                    ReturnValue += instruction.Item1 * instruction.Item2;
+                }
+            }
             return ReturnValue.ToString();
+        }
+        List<(int, int)> ParseMul(string input)
+        {
+            List<(int, int)> Parsed = new List<(int, int)>();
+            MatchCollection Matches = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)");
+            foreach (Match m in Matches)
+            {
+                Parsed.Add((Int32.Parse(m.Groups[1].Value), Int32.Parse(m.Groups[2].Value)));
+            }
+            return Parsed;
         }
     }
 }
