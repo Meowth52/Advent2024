7cf89cc07a45d57cf0ddbde58fbb811aefa00823
David Granberg <fearless_leader79@hotmail.com>
2024-12-07 14:13:15 +0100

    gg

--- Day07.cs
+++ Day07.cs
@@ -9,11 +9,11 @@ namespace Advent2024
 {
     public class Day07 : Day
     {
-        List<int> Instructions;
+        List<List<long>> Instructions;
         public Day07(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfLongerLists(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -21,14 +21,92 @@ namespace Advent2024
         }
         public string GetPartOne()
         {
-            int ReturnValue = 0;
+            long ReturnValue = 0;
+            foreach (List<long> equation in Instructions)
+            {
+                List<long> Sums = new List<long>();
+                Sums.Add(equation[1]);
+                bool Break = false;
+                for (int i = 2; i < equation.Count; i++)
+                {
+                    if (Sums.Count == 0)
+                        break;
+                    List<long> NextSums = new List<long>();
+                    foreach (long sum in Sums)
+                    {
+                        long plus = sum + equation[i];
+                        if (plus == equation[0] && i == equation.Count - 1)
+                        {
+                            ReturnValue += equation[0];
+                            Break = true;
+                            break;
+                        }
+                        long mul = sum * equation[i];
+                        if (mul == equation[0] && i == equation.Count - 1)
+                        {
+                            ReturnValue += equation[0];
+                            Break = true;
+                            break;
+                        }
 
+                        if (plus <= equation[0])
+                            NextSums.Add(plus);
+                        if (mul <= equation[0])
+                            NextSums.Add(mul);
+                    }
+                    Sums = new List<long>(NextSums);
+
+                }
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
-            int ReturnValue = 0;
+            long ReturnValue = 0;
+            foreach (List<long> equation in Instructions)
+            {
+                List<long> Sums = new List<long>();
+                Sums.Add(equation[1]);
+                bool Break = false;
+                for (int i = 2; i < equation.Count; i++)
+                {
+                    if (Sums.Count == 0)
+                        break;
+                    List<long> NextSums = new List<long>();
+                    foreach (long sum in Sums)
+                    {
+                        long plus = sum + equation[i];
+                        if (plus == equation[0] && i == equation.Count - 1)
+                        {
+                            ReturnValue += equation[0];
+                            Break = true;
+                            break;
+                        }
+                        long mul = sum * equation[i];
+                        if (mul == equation[0] && i == equation.Count - 1)
+                        {
+                            ReturnValue += equation[0];
+                            Break = true;
+                            break;
+                        }
+                        long fuu = sum*(long)Math.Pow(10,(long)Math.Log10( equation[i])+1)+ equation[i];
+                        if (fuu == equation[0] && i == equation.Count - 1)
+                        {
+                            ReturnValue += equation[0];
+                            Break = true;
+                            break;
+                        }
+                        if (plus <= equation[0])
+                            NextSums.Add(plus);
+                        if (mul <= equation[0])
+                            NextSums.Add(mul);
+                        if (fuu <= equation[0])
+                            NextSums.Add(fuu);
+                    }
+                    Sums = new List<long>(NextSums);
 
+                }
+            }
             return ReturnValue.ToString();
         }
     }
