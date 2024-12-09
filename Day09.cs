cc2fc74cd0372030751da56b2c146996b042271e
Meowth <NoThisIsAPublicRepo@No.No>
2024-12-09 16:43:38 +0100

    attans

--- Day09.cs
+++ Day09.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace Advent2024
 {
     public class Day09 : Day
     {
-        List<int> Instructions;
+        Dictionary<int, int> Disk;
+        int TopOne;
         public Day09(string _input) : base(_input)
         {
-            string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            string Input = this.CheckFile(_input).TrimEnd();
+            Disk = new Dictionary<int, int>();
+            TopOne = Input.Length - 1;
+            for (int i = 0; i < Input.Length; i++)
+            {
+                Disk[i] = Int32.Parse(Input[i].ToString());
+            }
         }
         public override Tuple<string, string> GetResult()
         {
@@ -21,8 +23,46 @@ namespace Advent2024
         }
         public string GetPartOne()
         {
-            int ReturnValue = 0;
+            long ReturnValue = 0;
+            long position = 0;
+            int topper = TopOne;
+            for (int i = 0; i < TopOne; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    long staph = position + Disk[i];
+                    for (int p = 0; p < Disk[i]; p++)
+                    {
+                        for (int t = topper; t == 0; t -= 2)
+                        {
+                            if (Disk[t] > 0)
+                            {
+                                Disk[t]--;
+                                ReturnValue += (t * (position + p)) / 2;
+                                if (ReturnValue >= 1928)
+                                    ;
+                                break;
+                            }
+                            else
+                                topper -= 2;
+                        }
+                    }
+                    position += Disk[i];
+                }
+                else
+                {
+                    int number = Disk[i];
+                    for (long p = 0; p < number; p++)
+                    {
+                        ReturnValue += (position + p) * i / 2;
+                        Disk[i]--;
+                    }
 
+                    if (ReturnValue >= 1928)
+                        ;
+                    position += Disk[i];
+                }
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
