aa24cd049d11e83a7b9c87663104779a2e247a74
Meowth <NoThisIsAPublicRepo@No.No>
2024-12-12 16:17:17 +0100

    a bit

--- Day12.cs
+++ Day12.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace Advent2024
 {
     public class Day12 : Day
     {
-        List<int> Instructions;
+        Dictionary<Coordinate, char> Map = new Dictionary<Coordinate, char>();
         public Day12(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Map = this.ParseCoordinateCharDic(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -30,6 +26,21 @@ namespace Advent2024
             int ReturnValue = 0;
 
             return ReturnValue.ToString();
+        }
+    }
+    public class Area
+    {
+        Coordinate Corner1;
+        Coordinate Corner2;
+        HashSet<Coordinate> IncludedCoordinates;
+        HashSet<Coordinate> ExludedCoordinates;
+        public Area(Coordinate First)
+        {
+            Corner1 = First;
+            Corner2 = First;
+            IncludedCoordinates = new HashSet<Coordinate>();
+            IncludedCoordinates.Add(First);
+            ExludedCoordinates = new HashSet<Coordinate>();
         }
     }
 }
