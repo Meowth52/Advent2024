906b01ac404e7d64d68c5fda5fee357d14c9ed5b
Meowth <NoThisIsAPublicRepo@No.No>
2024-12-17 17:32:53 +0100

    nope

--- Day15.cs
+++ Day15.cs
@@ -71,8 +71,117 @@ namespace Advent2024
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            Dictionary<Coordinate, char> map = new Dictionary<Coordinate, char>();
+            Coordinate player = null;
+            foreach (KeyValuePair<Coordinate, char> place in Map)
+            {
+                char left = '.';
+                char right = '.';
+                if (place.Value == 'O')
+                {
+                    left = '[';
+                    right = ']';
+                }
+                if (place.Value == '@')
+                    player = new Coordinate(place.Key);
+                if (place.Value == '#')
+                {
+                    left = '#';
+                    right = '#';
+                }
+                map.Add(new Coordinate(place.Key.x * 2, place.Key.y), left);
+                map.Add(new Coordinate(place.Key.x * 2 + 1, place.Key.y), right);
+            }
+            foreach (char c in Instructions)
+            {
+                Queue<Coordinate> queue = new Queue<Coordinate>();
+                queue.Enqueue(player);
+                Dictionary<Coordinate, char> maybe = new Dictionary<Coordinate, char>();
+                HashSet<Coordinate> remove = new HashSet<Coordinate>();
+                bool donk = false;
+                if (c == '<' || c == '>')
+                {
+                    while (queue.Count > 0)
+                    {
+                        Coordinate check = queue.Dequeue();
+                        remove.Add(check);
+                        Coordinate IsThisIt = check.LookAheadNSteps(c);
+                        if (map[IsThisIt] == '#')
+                        {
+                            donk = true;
+                            break;
+                        }
+                        if (map[IsThisIt] == '[')
+                        {
+                            if (!maybe.ContainsKey(IsThisIt))
+                                maybe.Add(new Coordinate(IsThisIt), ' ');
+                            maybe[IsThisIt] = map[check];
+                            queue.Enqueue(new Coordinate(IsThisIt));
+                        }
+                        if (map[IsThisIt] == ']')
+                        {
+                            if (!maybe.ContainsKey(IsThisIt))
+                                maybe.Add(new Coordinate(IsThisIt), ' ');
+                            maybe[IsThisIt] = map[check];
+                            queue.Enqueue(new Coordinate(IsThisIt));
+                        }
+                    }
+                }
+                else
+                {
+                    while (queue.Count > 0)
+                    {
+                        Coordinate check = queue.Dequeue();
+                        remove.Add(check);
+                        Coordinate IsThisIt = check.LookAheadNSteps(c);
+                        if (map[IsThisIt] == '#')
+                        {
+                            donk = true;
+                            break;
+                        }
+                        if (map[IsThisIt] == '[')
+                        {
+                            if (!maybe.ContainsKey(IsThisIt))
+                                maybe.Add(new Coordinate(IsThisIt), ' ');
+                            maybe[IsThisIt] = map[check];
+                            queue.Enqueue(new Coordinate(IsThisIt));
+                            queue.Enqueue(new Coordinate(IsThisIt.LookAheadNSteps('>')));
+                        }
+                        if (map[IsThisIt] == ']')
+                        {
+                            if (!maybe.ContainsKey(IsThisIt))
+                                maybe.Add(new Coordinate(IsThisIt), ' ');
+                            maybe[IsThisIt] = map[check];
+                            queue.Enqueue(new Coordinate(IsThisIt));
+                            queue.Enqueue(new Coordinate(IsThisIt.LookAheadNSteps('<')));
+                        }
+                    }
+                }
+                if (!donk)
+                {
+                    player.MoveNSteps(c);
+                    foreach (Coordinate r in remove)
+                    {
+                        map[r] = '.';
+                    }
+                    foreach (KeyValuePair<Coordinate, char> m in maybe)
+                    {
+                        map[m.Key] = m.Value;
+                    }
+                }
+            }
+            map[player] = '.';
+            int boxes = 0;
+            foreach (KeyValuePair<Coordinate, char> place in map)
+            {
+                if (place.Value == '[')
+                {
+                    boxes++;
+                    ReturnValue += place.Key.x + 100 * (YMax - place.Key.y);
+                }
+            }
             return ReturnValue.ToString();
+
         }
     }
 }
